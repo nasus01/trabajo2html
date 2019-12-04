b740b93e5f6ebfd95341fda702ab9dd59ad1da5e
christian morales <christianjack01@hotmail.com>
2019-12-04 02:03:33 -0500

    segundo taller de html

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name = "viewport" content="width=divice-width, initial-scale=1.0">
+    <title>
+        RIOT POITS FRIDAY
+    </title>
+    <link rel ="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<div id="pagewrap"> 
+     <center>
+        <div id="header">
+        <img id="logo" src="imagenes/Black_Friday.png">
+          <img id="logo2" src="imagenes/black_friday2.png">
+        <h1> Black friday</h1>
+        <p align=center>Aprovecha nuestras grandes promociones</p>  
+     </div>
+      </center>
+        <div id="menu">
+        <li class="listado"><a href="">Inicio</a></li>
+        <li class="listado"><a href="">Mision</a></li>
+        <li class="listado"><a href="">Vision</a></li>
+        <li class="listado"><a href="">Contactenos</a></li>
+          </div>
+
+        <div id="content">
+            <h2> <p style="color:red";> Grandes Promociones</h2>
+            
+            <div id=parrafo1>
+            <center>
+              <li><a  href="" >  skin para campeones</a></li>
+              
+              </center>
+            </div>
+           <div id=parrafo2>
+           <center>
+           <li><a  href="" >Cromos Trajes De Tu Campeon</a></li>
+           </center>
+            </div>
+            <div id=parrafo3>
+            <center>
+            <li><a  href="" >Gestos</a></li>
+            </center>
+           </div>
+           <div id=parrafo4>
+           <center>
+           <li><a  href="" >Campeones</a></li>
+           </center>
+           </div>
+           
+          
+
+
+
+        </div>
+      
+       
+        <div id="sidebar">
+
+
+            <h2><p style="color:green";>Seleciona El Metodo De Pago</h3>
+            <li><a  href="" >Visa</a></li>
+            <li><a  href="" >Master</a></li>
+            <li><a  href="" >>Paypal</a></li>
+
+
+        </div>
+        <div id="sidebar1">
+
+
+            <h2><p style="color:green";>Accesorios</h3>
+            <li><a  href="" >Centinelas</a></li>
+            <li><a  href="" >iconos</a></li>
+            <li><a  href="" >Tarjetas De Regalo</a></li>
+
+
+        </div>
+        <center>
+        <div id="footer">
+        <img id="imagen3" src="imagenes/black_friday2.png">
+        <img id="imagen4" src="imagenes/League_of_legends_logo_transparent.png">
+
+      
+
+        <p>
+            Contactenos:
+            
+</p>
+<li><a  href="" >cristianjak01@gmail.com</a></li>
+
+<p>christian morales loaiza
+</p>
+<p> Analisis y Desarrollo de Software - censa
+        </p>
+        
+        </div>
+        
+            
+        
+      </center>
+    
+
+
+
+    </div>
+</body>
+</html>
